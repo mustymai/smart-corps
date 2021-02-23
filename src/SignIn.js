a3e00e4f5e63e1cb65af938e9f1e489d5b5b16cb
mustymai <mai.mustafa08@gmail.com>
2021-02-22 18:10:47 -0800

    creating demo app for smart corps

---
+++ src/SignIn.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SignIn = () => {
+    return(
+        <div>
+
+            <h1>
+            this is the SignIn 
+            </h1>
+        </div>
+
+
+
+    )
+}
+
+
+
+
+export default SignIn;
